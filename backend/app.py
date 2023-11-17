ff44cd58617ef7d70970dd345ad91b4289abe323
Daisy Chang <146611553+daisychangm@users.noreply.github.com>
2023-11-17 16:52:47 -0500

    Added routes for users

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import json
 
-from db import db
-from flask import Flask
+from db import *
+from flask import Flask, request
 import os
 
 # define db filename
@@ -31,6 +31,65 @@ def failure_response(message, code=404):
 @app.route("/")
 @app.route("/api/")
 
+# routes for users
+@app.route("/api/users/")
+def get_users():
+    """
+    Endpoint for getting all users
+    """
+    users = [user.serialize() for user in User.query.all()]
+    return success_response({"users": users}, 200)
+
+@app.route("/api/users/", methods = ["POST"])
+def create_user():
+    """
+    Endpoint for creating a new user
+    """
+    body = json.loads(request.data)
+    name = body.get("name")
+    netid = body.get("netid")
+    balance = body.get("balance", 0.0)
+    if (name is None) or (netid is None):
+        return failure_response("Invalid name or netid.", 400)
+    new_user = User(name = name, netid = netid, balance = balance)
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+@app.route("/api/users/<int:user_id>/")
+def get_user(user_id):
+    """
+    Endpoint for getting a user by id
+    """
+    user = User.query.filter_by(id = user_id).first()
+    if user is None:
+        return failure_response("User not found.")
+    return success_response(user.serialize())
+
+@app.route("/api/users/<int:user_id>/")
+def update_user(user_id):
+    user = User.query.filter_by(id = user_id).first()
+    if user is None:
+        return failure_response("User not found.")
+    body = json.loads(request.data)
+    name = body.get("name", user.name)
+    balance = body.get("balance", user.balance)
+    user.name = name
+    user.balance = balance
+    db.session.commit()
+    return success_response(user.serialize())
+
+@app.route("/api/users/<int:user_id>/", methods = ["DELETE"])
+def delete_user(user_id):
+    """
+    Endpoint for deleting a user by id
+    """
+    user = User.query.filter_by(id = user_id).first()
+    if user is None:
+        return failure_response("User not found.")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
