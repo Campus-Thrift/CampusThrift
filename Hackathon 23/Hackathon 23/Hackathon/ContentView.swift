2afd8f665906b99905d5e4c8d729bc966320ff5b
Naakai <iakaanb@gmail.com>
2023-11-30 23:43:32 -0500

    Almost Done

---
+++ Hackathon 23/Hackathon 23/Hackathon/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hackathon 23
+//
+//  Created by kai on 11/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
